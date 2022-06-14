c72ccac712898ea28ec1d68973699de69e43d0e6
Nirav1424 <niravasodariya7787@gmail.com>
2022-06-14 22:42:48 +0530

    Updates

---
+++ precache-manifest.c424afa7d9c804db59bbef437699c96a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6963d528d6b36b1c511c",
+    "url": "/utility/static/css/main.87729cde.chunk.css"
+  },
+  {
+    "revision": "6963d528d6b36b1c511c",
+    "url": "/utility/static/js/main.6963d528.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/utility/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/utility/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "0df06887c124f85c32e1",
+    "url": "/utility/static/js/runtime~main.0df06887.js"
+  },
+  {
+    "revision": "1584af8fdcb7daa4f64d1a1982d9e3e0",
+    "url": "/utility/index.html"
+  }
+];
